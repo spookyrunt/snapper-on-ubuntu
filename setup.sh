6dfeca8fc62d515de7ff0cddd68db7e997d070ab
u <>
2026-09-12 03:51:41 +0900

    convenience

--- setup.sh
+++ setup.sh
@@ -68,7 +68,7 @@ END {
         print root_dev, "/.snapshots", "btrfs",
               "subvol=/.snapshots,defaults,noatime,compress=zstd", "0", "0"
 }' /etc/fstab |
-sudo tee /tmp/fstab >/dev/null
+  sudo tee /tmp/fstab >/dev/null
 sudo mv /tmp/fstab /etc/fstab
 
 echo "Reloading systemd manager configuration..."
@@ -76,10 +76,10 @@ systemctl daemon-reload
 
 echo "Applying new mount options..."
 mount -a || {
-    echo "mount -a failed! Restoring fstab from backup."
-    sudo cp "$FSTAB_BACKUP" /etc/fstab
-    sudo systemctl daemon-reload
-    exit 1
+  echo "mount -a failed! Restoring fstab from backup."
+  sudo cp "$FSTAB_BACKUP" /etc/fstab
+  sudo systemctl daemon-reload
+  exit 1
 }
 
 echo "--- Current Btrfs Mount Status ---"
@@ -121,7 +121,7 @@ systemctl enable --now snapper-timeline.timer
 systemctl enable --now snapper-cleanup.timer
 
 echo "Creating initial verification snapshot..."
-snapper -c root create -d "Initial automated setup"
+snapper -c root create -d "automated setup" -c number
 
 #################################################
 # PART 4: Final verification
